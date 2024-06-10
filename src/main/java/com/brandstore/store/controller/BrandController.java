3a6b5fd889ca6f591326a9b820455c78a8041fef
Amandabgoes <amandabriganti15@hotmail.com>
2024-06-10 10:43:21 -0300

    Criação de classes de infraestrutura

---
+++ src/main/java/com/brandstore/store/controller/BrandController.java
@@ -0,0 +1,20 @@
+package com.brandstore.store.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.brandstore.store.service.BrandService;
+
+@Controller
+@RequestMapping("/clothings")
+public class BrandController {
+	
+	@Autowired
+	private BrandService brandService;
+	
+	private BrandService getBrandService() {
+		return brandService;
+	}
+
+}
---
+++ src/main/java/com/brandstore/store/service/BrandService.java
@@ -0,0 +1,20 @@
+package com.brandstore.store.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brandstore.store.repository.BrandRepository;
+
+@Service
+public class BrandService {
+	
+	@Autowired
+	private BrandRepository brandRepository;
+	
+	
+	private BrandRepository getBrandRepository() {
+		return brandRepository;
+	}
+	
+
+}
