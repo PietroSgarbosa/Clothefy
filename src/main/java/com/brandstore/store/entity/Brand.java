bc26c72df04a1a07a15a91ae88ee674a5ca8d66d
Amandabgoes <amandabriganti15@hotmail.com>
2024-06-10 10:22:53 -0300

    Criação de entidade

---
+++ src/main/java/com/brandstore/store/entity/Brand.java
@@ -0,0 +1,61 @@
+package com.brandstore.store.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="BRAND")
+public class Brand {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="NAME", nullable = false)
+	private String name;
+	
+	@Column(name="ADRESS")
+	private String adress;
+	
+	@Column(name="PHONE")
+	private String phone;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/brandstore/store/entity/Category.java
@@ -0,0 +1,65 @@
+package com.brandstore.store.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="CATEGORY")
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="NAME", nullable = false)
+	private String name;
+	
+	@Column(name="DESCRIPTION")
+	private String description;
+	
+	@OneToMany(mappedBy="category")
+	private List<Clothing> clothings;
+	
+
+	public List<Clothing> getClothings() {
+		return clothings;
+	}
+
+	public void setClothings(List<Clothing> clothings) {
+		this.clothings = clothings;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
