7ffce6ecea1e7c7d8d95048befebae7a163d2d43
luiz213 <luiz.oliveira@simples.software>
2024-06-25 12:26:26 -0300

    Test de GetAll e GetById

--- src/test/java/com/brandstore/store/test/BrandControllerTest.java
+++ src/test/java/com/brandstore/store/test/BrandControllerTest.java
@@ -15,40 +15,36 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.brandstore.store.controller.BrandController;
 import com.brandstore.store.dto.BrandDTO;
+import com.brandstore.store.entity.Brand;
 import com.brandstore.store.service.BrandService;
 
 @SpringBootTest
 public class BrandControllerTest {
 	
-	// MEMORIZAR PADRÂO DE FUNCIONALIDADE ---------------//
-	//MOCKANDO O SERVICE, isso são os dados mockados
 	@Mock
 	@Autowired
-	private BrandService brandService; // -> MOCKAR
+	private BrandService brandService; 
 	
-	//INJETANDO OS DADOS MOCKADOS na classe que nós vamos testar
 	@InjectMocks
 	@Autowired
-	private BrandController brandController; // -> TESTAR
+	private BrandController brandController; 
 	
 	@BeforeEach
 	void setUp() {
 		MockitoAnnotations.openMocks(this);
 	}
-	// --------------------------------------------------//
-	
-	//setando constantes pra evitar redundância
+
 	private static final Long BRAND_ID = 1l;
 	private static final Long BRAND_ID2 = 2l;
 	
 	@SuppressWarnings("unchecked")
 	@Test
 	void testGetAll() {
-		//Arrange - organizar ou preparar os dados mockados -----------//
 		BrandDTO brand1 = new BrandDTO();
 		brand1.setId(BRAND_ID);
 		brand1.setName("Adidas");
@@ -59,21 +55,51 @@ public class BrandControllerTest {
 	
 		List<BrandDTO> listBrandDTO = Arrays.asList(brand1, brand2);
 		
-		//Troca o serviço real pelo mockado e traz os dados que preparamos para teste
 		when(brandService.getAll()).thenReturn(listBrandDTO);
-		// ------------------------------------------------------------//
 		
-		//Act - chamar o método que vai ser testado (chamar getAll)
 		ResponseEntity<List<BrandDTO>> testResponse = (ResponseEntity<List<BrandDTO>>) brandController.getAll();
 		
-		//Assert - verificar ou validar o teste
 		assertThat(testResponse.getBody()).hasSize(2);
 		assertThat(testResponse.getBody().get(0).getName()).isEqualTo("Adidas");
 		assertThat(testResponse.getBody().get(1).getName()).isEqualTo("Puma");
 		verify(brandService, times(1)).getAll(); 
 	}
 	
-	//Fazer um teste do getById e como bonus, fazer o teste da exceção do getAll
-	//Começar aqui
+	@Test
+	void testGetAllException() {
+		when(brandService.getAll()).thenThrow(new RuntimeException("Serviço Indisponivel"));
+		
+		ResponseEntity<?> testResponse = brandController.getAll();
 
+        assertThat(testResponse.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+        assertThat(testResponse.getBody()).isEqualTo("Internal Server Error: Serviço Indisponivel");
+        verify(brandService, times(1)).getAll();
+	}
+	
+	@Test
+	void testGetById() {
+		Brand brand = new Brand();
+		brand.setId(BRAND_ID);
+		brand.setName("Adidas");
+		
+		when(brandService.getById(BRAND_ID)).thenReturn(brand);
+		
+		
+		ResponseEntity<Brand> testResponse = (ResponseEntity<Brand>) brandController.getById(BRAND_ID);
+		
+		assertThat(testResponse.getBody().equals(brand));
+		verify(brandService, times(1)).getById(BRAND_ID);
+	
+	}
+	
+	@Test
+	void testGetByIdException() {
+		when(brandService.getById(BRAND_ID)).thenThrow(new RuntimeException("Serviço Indisponivel"));
+		
+		ResponseEntity<?> testResponse = brandController.getById(BRAND_ID);
+
+        assertThat(testResponse.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+        assertThat(testResponse.getBody().equals("Serviço Indisponivel"));
+        verify(brandService, times(1)).getById(BRAND_ID);
+	}
 }
