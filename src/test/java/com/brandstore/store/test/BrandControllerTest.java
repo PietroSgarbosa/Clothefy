6a9afca030ec4978dbc4ae93d7e6cc1cded4cf4a
LuizPerego97 <luiz.perego@simples.software>
2024-06-25 14:25:25 -0300

    Teste unitario GetById, teste unitario com exception de GetById e getALL

--- src/test/java/com/brandstore/store/test/BrandControllerTest.java
+++ src/test/java/com/brandstore/store/test/BrandControllerTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,10 +16,12 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.brandstore.store.controller.BrandController;
 import com.brandstore.store.dto.BrandDTO;
+import com.brandstore.store.entity.Brand;
 import com.brandstore.store.service.BrandService;
 
 @SpringBootTest
@@ -73,7 +76,59 @@ public class BrandControllerTest {
 		verify(brandService, times(1)).getAll(); 
 	}
 	
-	//Fazer um teste do getById e como bonus, fazer o teste da exceção do getAll
-	//Começar aqui
+	@Test
+	void testGetAll_WhenControllerThrowsException() {
+
+	    // Simula uma exceção ao chamar o serviço
+	    when(brandService.getAll()).thenThrow(new RuntimeException("Nenhum dado encontrado"));
+
+	    // Act - chamar o método que vai ser testado (chamar getAll)
+	    ResponseEntity<?> testResponse = brandController.getAll();
+
+	    // Assert - verificar se a exceção foi lançada corretamente
+	    assertThat(testResponse.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+	    verify(brandService, times(1)).getAll();
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	void testGetById() {
+	    // Arrange - organizar ou preparar os dados mockados
+	    Brand brand1 = new Brand();
+	    brand1.setId(BRAND_ID);
+	    brand1.setName("Adidas");
+	    
+	    Brand brand2 = new Brand();
+		brand2.setId(BRAND_ID2);
+		brand2.setName("Puma");
+
+	    // Troca o serviço real pelo mockado e traz os dados que preparamos para teste
+	    when(brandService.getById(BRAND_ID2)).thenReturn(brand2);
+
+	    // Act - chamar o método que vai ser testado (chamar getById)
+	    ResponseEntity<Brand> testResponse = (ResponseEntity<Brand>) brandController.getById(BRAND_ID2);
+
+	    // Assert - verificar ou validar o teste
+	    assertThat(testResponse.getBody()).isNotNull(); 
+	    assertThat(testResponse.getBody().getName()).isEqualTo("Puma"); 
+	    assertThat(testResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+	    verify(brandService, times(1)).getById(BRAND_ID2); 
+	}
+	
+	@Test
+	void testGetById_WhenControllerThrowsException() {
+	    // Simula uma exceção ao chamar o serviço
+	    when(brandService.getById(1L)).thenThrow(new RuntimeException("Nenhum dado encontrado"));
+
+	    // Chama o método que vai ser testado (chamar getById)
+	    ResponseEntity<?> testResponse = brandController.getById(1L);
+
+	    // Verifica se a exceção foi lançada corretamente
+	    assertThat(testResponse.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+	    verify(brandService, times(1)).getById(1L);
+	}
+
+
 
 }
